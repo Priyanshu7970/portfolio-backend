08916fd1105302ccdbbb6537ebc08f73ee4e91fb
Priyanshu <priyanshugupta8720@gmail.com>
2025-04-28 16:42:29 +0530

    This is inital of sanity backend

--- schemas/index.js
+++ schemas/index.js
@@ -1,4 +1,5 @@
 import blog from "./blog" 
 import author from "./author"
 import profile from "./profile"
+// This is where we can define our schemas for our pages
 export const schemaTypes = [blog,author,profile]
